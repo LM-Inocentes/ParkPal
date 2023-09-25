3d9832a2c8fe34b4cd5418b5042584e9e454b159
itsJozieMerrie <josemaribering@gmail.com>
2023-09-25 19:24:44 +0800

    more buttons

--- ParkPal/src/app/app-routing.module.ts
+++ ParkPal/src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './component/login/login.component';
 import { RegisterComponent } from './component/register/register.component';
+import { AppNavigationComponent } from './app-navigation/app-navigation.component';
 
 const routes: Routes = [
   {path : '',title: 'ParkPal-Login',  component : LoginComponent},
   {path : 'register',title: 'Parkpal-register',  component : RegisterComponent},
+  {path : 'app-navigation', title: 'App Navigation', component : AppNavigationComponent}
 ];
 
 @NgModule({
