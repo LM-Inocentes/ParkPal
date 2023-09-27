e6f7368f3512a5f935ffe1f0687e30ae3b4826f0
itsJozieMerrie <josemaribering@gmail.com>
2023-09-27 23:01:12 +0800

    dashboard progress

--- ParkPal/src/app/dashboard/dashboard.component.ts
+++ ParkPal/src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../services/auth.service';
+import { Admin } from '../shared/models/admin';
+
 
 @Component({
   selector: 'app-dashboard',
@@ -6,5 +9,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent {
+  admin!:Admin;
+  Firstname?:string;
 
+  constructor(private authService:AuthService) {
+    authService.userObservable.subscribe((newAdmin) => {
+      this.admin = newAdmin;
+      if(this.isAuth){
+        this.Firstname = this.admin.Fullname.split(' ').at(0);
+        console.log(this.admin);
+      }
+    });
+  }
+  
+  get isAuth(){
+    return this.admin.token;
+  }
 }
