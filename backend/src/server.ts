d224ecd9b07c95d0a3db783c21c9ba584a4713e4
LM-Inocentes <louisemartininocentes@gmail.com>
2023-09-29 18:18:35 +0800

    minor edit

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -16,12 +16,12 @@ const app = express();
 
 app.use(express.json());
 app.use(cors({
-    credentials: true,
-    origin:["http://localhost:4200"],
-}));
+    origin: 'https://parkpal.netlify.app'
+  }));
 
 app.use("/api/users/", userRouter);
 app.use("/api/uploads/", uploadRouter);
+
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
