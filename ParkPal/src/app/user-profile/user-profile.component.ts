3f50d0310d61abfe66e95c9e4169190d1064320b
LM-Inocentes <louisemartininocentes@gmail.com>
2023-11-05 23:50:35 +0800

    user profile conditionals and backend integration

--- ParkPal/src/app/user-profile/user-profile.component.ts
+++ ParkPal/src/app/user-profile/user-profile.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { AuthService } from '../services/auth.service';
 import { User } from '../shared/models/user';
+import { ImageModalComponent } from '../component/image-modal/image-modal.component';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-user-profile',
@@ -10,21 +12,34 @@ import { User } from '../shared/models/user';
 export class UserProfileComponent {
   panelOpenState = false;
   user!:User;
-  Firstname?:string;
-  userID?:string;
+  intitials!: string;
 
-  constructor( authService:AuthService ) {
+  constructor( authService:AuthService, private dialog: MatDialog ) {
     authService.userObservable.subscribe((newUser) => {
       this.user = newUser;
-      if(this.isAuth){
-        this.Firstname = this.user.Fullname.split(' ').at(0);
-        console.log(this.user);
-        this.userID = this.user.id.split(' ').at(0);;
-      }
+      this.calculateInitials();
     });
   }
   
-  get isAuth(){
-    return this.user.token;
+  calculateInitials() {
+    if (this.user && this.user.Fullname) {
+      const nameParts = this.user.Fullname.split(' ');
+      let initials = '';
+
+      nameParts.forEach(part => {
+        if (part.length > 0) {
+          initials += part[0].toUpperCase();
+        }
+      });
+
+      // Now 'initials' contains the user's initials.
+      this.intitials = initials;
+    }
   }
+  openImageModal(imageUrl: string) {
+    this.dialog.open(ImageModalComponent, {
+      data: { imageUrl },
+    });
+  }
+
 }
