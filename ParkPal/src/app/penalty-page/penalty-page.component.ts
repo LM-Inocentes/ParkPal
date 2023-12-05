5fcd4daa828c991c70a53fee881ed828d30e9e45
Murasaki-Vien <neil.melvin.diaz@gmail.com>
2023-12-06 00:09:23 +0800

    help

--- ParkPal/src/app/penalty-page/penalty-page.component.ts
+++ ParkPal/src/app/penalty-page/penalty-page.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { User } from '../shared/models/user';
+import { AuthService } from '../services/auth.service';
+import { Observable } from 'rxjs';
+
 
 @Component({
   selector: 'app-penalty-page',
@@ -6,7 +10,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./penalty-page.component.scss']
 })
 export class PenaltyPageComponent {
+  RegisteredUserList: User[] = [];
 
+  constructor(
+    private authService:AuthService,
+  ) {}
  
+  ngOnInit(): void{
+    let RegisteredUserListObservable!: Observable<User[]>;
+
+    
+    RegisteredUserListObservable.subscribe((RegisteredUserList) => {
+      this.RegisteredUserList = this.RegisteredUserList;
+    }
+    )
+   
+
+    
+  }
 
 }
